d7c09fbba01a028fe91dc95b8eabb71270610783
real879 <renatto879@hotmail.com>
2021-11-02 23:28:07 -0500

    Add pokemon list view

---
+++ app/src/main/java/com/example/pokeapp/PokemonListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pokeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PokemonListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_list)
+    }
+}
