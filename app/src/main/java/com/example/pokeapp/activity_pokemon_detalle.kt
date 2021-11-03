339571c9c6a1347c2dd397ae97d506df1896ae0e
FernandoCandela <20171937@aloe.ulima.edu.pe>
2021-11-03 00:47:24 -0500

    add pokemon detalle

---
+++ app/src/main/java/com/example/pokeapp/activity_pokemon_detalle.kt
@@ -0,0 +1,11 @@
+package com.example.pokeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_pokemon_detalle : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_detalle)
+    }
+}
