409c9edfd556dd397ecaad46d6af37da609d8081
FernandoCandela <20171937@aloe.ulima.edu.pe>
2021-11-14 18:26:04 -0500

    add list fav observer

--- app/src/main/java/com/example/pokeapp/FavoritosListActivity.kt
+++ app/src/main/java/com/example/pokeapp/FavoritosListActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.pokeapp.fragment.PokemonFavoritoFragment
 import com.example.pokeapp.fragment.PokemonFragment
 import com.example.pokeapp.model.Pokemon2
@@ -12,12 +14,21 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class FavoritosListActivity : AppCompatActivity(), PokemonFavoritoFragment.OnPokemonSelectedListener{
-
+    lateinit var viewModel: PokemonFavManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favoritos_list)
 
-        /*PokemonFavManager().getPokemonsFavByUserFirebase("fernando")*/
+
+        viewModel = ViewModelProvider(this).get(PokemonFavManager::class.java)
+
+        viewModel.getPokemonsFavByUserFirebase("fernando")
+        viewModel.favoritos.observe(this, Observer { pokemon ->
+            for (p in pokemon){
+                print(p.name_entrenador)
+                print(p.name_pokemon)
+            }
+        })
         val fragment = PokemonFavoritoFragment()
         val ft = supportFragmentManager.beginTransaction()
         ft.add(R.id.fraFavoritos,fragment)
