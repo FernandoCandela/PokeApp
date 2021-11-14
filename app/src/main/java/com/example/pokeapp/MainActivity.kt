ee9a16a45cded5d9630c3585a209a1230fd3824d
FernandoCandela <20171937@aloe.ulima.edu.pe>
2021-11-13 20:35:43 -0500

    add entrenador

--- app/src/main/java/com/example/pokeapp/MainActivity.kt
+++ app/src/main/java/com/example/pokeapp/MainActivity.kt
@@ -4,25 +4,55 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 /*Fernando Candela maldonado
 * Renatto Pantoja*/
 class MainActivity : AppCompatActivity() {
+    private val dbFirebase = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val butContinuar: Button = findViewById(R.id.butContinuar)
         val butFav: Button = findViewById(R.id.butFavoritos)
+
         butContinuar.setOnClickListener { _: View ->
-            val intent: Intent = Intent()
-            intent.setClass(this, PokemonListActivity::class.java)
-            startActivity(intent)
+            if(entrenadorIsValid()){
+                addEntrenador()
+                val intent: Intent = Intent()
+                intent.setClass(this, PokemonListActivity::class.java)
+                startActivity(intent)
+            }
         }
         butFav.setOnClickListener { _: View ->
-            val intent: Intent = Intent()
-            intent.setClass(this, FavoritosListActivity::class.java)
-            startActivity(intent)
+            if(entrenadorIsValid()){
+                addEntrenador()
+                val intent: Intent = Intent()
+                intent.setClass(this, FavoritosListActivity::class.java)
+                startActivity(intent)
+            }
         }
+    }
+
+    fun entrenadorIsValid(): Boolean {
+        val ptxEntrenador: EditText = findViewById(R.id.ptxEntrenador)
+        if (!(ptxEntrenador.text).isNullOrEmpty()) {
+            return true
+        } else {
+            Toast.makeText(this, "Ingresa un Entrenador", Toast.LENGTH_SHORT).show()
+            return false
+        }
+    }
+    fun addEntrenador() {
+        val ptxEntrenador: EditText = findViewById(R.id.ptxEntrenador)
+        dbFirebase.collection("entrenador")
+            .document(ptxEntrenador.text.toString())
+            .set(hashMapOf("name" to ptxEntrenador.text.toString()))
     }
 }
--- app/src/main/java/com/example/pokeapp/model/PokemonManager.kt
+++ app/src/main/java/com/example/pokeapp/model/PokemonManager.kt
@@ -1,72 +1,11 @@
 package com.example.pokeapp.model
 
 import android.content.Context
-import android.util.Log
-import androidx.room.Room
-import com.example.pokeapp.network.APIPokemonService
-import com.example.pokeapp.room.VGAppDatabase
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class PokemonManager(context: Context) {
-
-
-    private val db = Room.databaseBuilder(
-        context,
-        VGAppDatabase::class.java, "db_pokemon"
-    ).allowMainThreadQueries().build()
-
     private val dbFirebase = Firebase.firestore
-
-    private val API_URL = "https://pokeapi.co/api/v2/"
-
-    private val retrofit: Retrofit = Retrofit.Builder()
-        .baseUrl(API_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
-
-    private val service: APIPokemonService = retrofit.create(APIPokemonService::class.java)
-
-
-    fun getPokemonList(callbackOK: (PokeApiResponse) -> Unit, callbackError: (String) -> Unit) {
-        val call = service.getPokemonList(20, 0)
-        call.enqueue(object : Callback<PokeApiResponse> {
-            override fun onResponse(
-                call: Call<PokeApiResponse>,
-                response: Response<PokeApiResponse>
-            ) {
-                callbackOK(response.body()!!)
-            }
-
-            override fun onFailure(call: Call<PokeApiResponse>, t: Throwable) {
-                Log.e("PokemonManager", t.message!!)
-                callbackError(t.message!!)
-            }
-        })
-    }
-
-    fun getPokemonStats(
-        callbackOK: (Pokemon) -> Unit,
-        callbackError: (String) -> Unit,
-        name: String
-    ) {
-        val call = service.getPokemonStats(name)
-        call.enqueue(object : Callback<Pokemon> {
-            override fun onResponse(call :Call<Pokemon>, response: Response<Pokemon>) {
-                callbackOK(response.body()!!)
-            }
-
-            override fun onFailure(call: Call<Pokemon>, t: Throwable) {
-                Log.e("PokemonManager", t.message!!)
-                callbackError(t.message!!)
-            }
-        })
-    }
 
     fun getPokemonsFirebase(callbackOK : (List<Pokemon2>) -> Unit, callbackError : (String) -> Unit) {
         dbFirebase.collection("pokemon")
@@ -95,14 +34,5 @@ class PokemonManager(context: Context) {
     }
 
 
-    fun getProductsByRoom(callbackOK: (List<Pokemon2>) -> Unit, callbackError: (String) -> Unit) {
-        val pokemon: List<Pokemon2> = db.pokemonDAO().findAll()
-        callbackOK(pokemon)
-    }
 
-    private fun saveIntoRoom(pokemons: List<Pokemon2>) {
-        pokemons.forEach {
-            db.pokemonDAO().insert(it)
-        }
-    }
 }
